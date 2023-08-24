bf73e0ee265870fca79cf070234608b8add25a41
Alejandro Sosa <sosaa9302@gmail.com>
2023-08-24 18:58:53 -0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -39,20 +39,24 @@ function showList(array) {
 document.addEventListener("DOMContentLoaded", (e) => {
   // Escribe tu solución aquí
   // Sugerencia de cómo mostrar el array => showList(strangeArray);
+
+  // Filtra solo strings
   function string(element){
     return typeof element === "string";
   }
   const filteredArray = strangeArray.filter(string);
 
+// Convierte la primera letra de cada string a mayúscula
   const capitalizedArray = filteredArray.map((element) => {
     if (typeof element === "string" && element.length>0){
       return element.charAt(0).toUpperCase() + element.slice(1);
     }
     return element;
   });
-
+// Ordena alfabéticamente 
   const sortedArray = capitalizedArray.slice().sort();
 
+// Muestra el array filtrado, convertido y ordenado  
   showList(sortedArray);
 });
   
